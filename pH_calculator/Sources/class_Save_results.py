4f3289b2b15dee6c40819a017a6fcaffd21be183
Janbinkowski96 <jan.binkowski96@gmail.com>
2019-02-25 18:07:15 +0100

    first commit

--- pH_calculator/Sources/class_Save_results.py
+++ pH_calculator/Sources/class_Save_results.py
@@ -4,7 +4,7 @@ class Save_results():
         
         file_1 = open("results_ph_calculator.txt", "a")
         file_1.write("""
-        \n>calculations number {}
+        >calculations number {}
         {} buffer prepared using: acid volume {} [dm^3], base volume {} [dm^3], acid concentration {} [dmol/dm^3], base concentration {} [mol/dm^3]
         results:
         ionic strength {}, pH with correction {}, ph {}""".format(y, buff_name, V1, V2, c1, c2, u, Ph, Ph0))
@@ -14,7 +14,7 @@ class Save_results():
         
         file_2 = open("results_buffer_recipt.txt", "a")
         file_2.write("""
-        \n>calculations number {}
+        >calculations number {}
         {} buffer parameters: volume {} [dm3], pH {}, concentration {} [mol/dm^3]
         buffer recipt: basic component {} [mol], acid component {} [mol], fill water to {}
         """.format(z, buff_name, V, pH, C, Mcso, Mcko, V)) 
